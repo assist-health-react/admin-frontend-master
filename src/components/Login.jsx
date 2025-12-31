afd0500b2d45b8e2f4bc277111cc2dc15a40c1c5
Sivakumar <balusiva2312@gmail.com>
2025-12-31 19:31:36 +0530

    Success For CD

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -172,7 +172,7 @@ const Login = ({ onLogin }) => {
             />
           </div>
           <p className="mt-2 text-sm text-gray-600">
-            Sign in to your admin account -- TEst Data
+            Sign in to your admin account 
           </p>
         </div>
 
