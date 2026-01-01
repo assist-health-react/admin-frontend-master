bdb590d9087e022e4257c384254759fe297ce2af
Sivakumar <balusiva2312@gmail.com>
2026-01-01 21:06:12 +0530

    New Api route change

--- src/services/authService.js
+++ src/services/authService.js
@@ -2,6 +2,7 @@ import api from './api';
 
 export const login = async (email, password) => {
   try {
+    
     const response = await api.post('/api/v1/auth/login', {
       email,
       password
